a8013c906aee273c436b7ba9356b74b49e9ffbfa
jaronrpollman2 <jaron.r.pollman.2@ndsu.edu>
2016-10-31 20:00:01 -0500

    Classes added from AppEngine Group
    Adds some java classes to the module for the app's engine

---
+++ App_Engine/src/main/java/com/example/Date.java
@@ -0,0 +1,26 @@
+package com.example;
+
+/**
+ *
+ * @author donovan.beckmann
+ */
+public interface Date {
+    public int getYear();
+    public void setYear(int yyyy);
+    
+    public int getMonth();
+    public void setMonth(int mm);
+    
+    public int getDay();
+    public void setDay(int dd);
+    
+    public int getHour();
+    public void setHour(int hh);
+    
+    public int getMinute();
+    public void setMinute(int mm);
+    
+    public String toLongString();
+    public String toString();
+    public boolean equals(Object o); //MealDate doesn't use this method yet, Ask Ryan
+}
