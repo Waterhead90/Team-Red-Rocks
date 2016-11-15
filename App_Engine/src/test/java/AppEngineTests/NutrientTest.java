57678dbb404965d17c89606ac257111de7b2ac3c
ryanquinnnelson <ryan.q.nelson@gmail.com>
2016-11-14 23:31:51 -0600

    Removed folder Jaron created for unit tests, let Android create folder in proper location. Added unit test for single Nutrient method to check if it works.

---
+++ App_Engine/src/test/java/AppEngineTests/NutrientTest.java
@@ -0,0 +1,31 @@
+package AppEngineTests;
+import com.Engine.Nutrient;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ryan on 11/14/16.
+ */
+public class NutrientTest {
+    @org.junit.Before
+    public void setUp()  {
+
+    }
+
+    @org.junit.After
+    public void tearDown()  {
+
+    }
+
+    @org.junit.Test
+    public void testToString()
+    {
+
+        Nutrient test = new Nutrient("Protein", "grams");
+        String expected = "nutrient{" + "name =" + "Protein" + ", unit =" + "grams" + "}";
+        String actual = test.toString();
+
+        assertEquals(expected, actual);
+    }
+
+}
