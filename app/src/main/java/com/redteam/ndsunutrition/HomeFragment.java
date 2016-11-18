8d81cbda87553794ed50aab6e7bf9dec850b0054
jaronrpollman2 <jaron.r.pollman.2@ndsu.edu>
2016-11-17 21:06:51 -0600

    Fixed the string.xml And added spinner to home screen
    Fixed the issue with the strings.xml and added a working spinner to the
    home page fragment that (currently) displays times, but can be adjusted
    to display whatever

--- app/src/main/java/com/redteam/ndsunutrition/HomeFragment.java
+++ app/src/main/java/com/redteam/ndsunutrition/HomeFragment.java
@@ -10,12 +10,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
-public class HomeFragment extends Fragment
+public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener
 {
     private static final String ARG_SECTION_NUMBER = "section_number";
     TextView tv;
@@ -36,33 +39,54 @@ public class HomeFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        //The current fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        //Create reference to Spinner and setup adapter
+        Spinner selectionSpinner = (Spinner) rootView.findViewById(R.id.selectionSpinner);
+        ArrayAdapter spinnerAdapter = ArrayAdapter.createFromResource(getContext(),R.array.time_array,android.R.layout.simple_spinner_item); // using time Array as a placeholder
+        selectionSpinner.setAdapter(spinnerAdapter);
+        selectionSpinner.setOnItemSelectedListener(this);
+
+        //Create list view and adapter
         ListView list = (ListView) rootView.findViewById(R.id.recommendationList);
         RecommendationAdapter listAdapter = new RecommendationAdapter(getContext(),R.layout.fragment_recommendation_item);
         list.setAdapter(listAdapter);
 
+        //Iteration to add elements to the List
         for(int i = 0; i != 5; i++)
         {
-            listAdapter.add("Generic Placeholder");
+            listAdapter.add("");
         }
 
-
+        //Create middle progress bar
         Resources res = getResources();
         Drawable drawable = res.getDrawable(R.drawable.circular);
         final ProgressBar mProgress = (ProgressBar) rootView.findViewById(R.id.circularProgressbar);
         int progress = 4000;
 
-
+        //Temp code to show progress bar is working
         mProgress.setMax(4000); // Maximum Progress
         mProgress.setSecondaryProgress(4000); // Secondary Progress
         mProgress.setProgressDrawable(drawable);
 
+        //Text View in center of progress bar
         tv = (TextView) rootView.findViewById(R.id.tv);
 
+        //Temp code to set the current progress
         mProgress.setProgress(progress);
-
         tv.setText("" + progress);
+
         return rootView;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
