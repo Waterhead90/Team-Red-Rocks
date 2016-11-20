9151e757592680a9581762f2b1f8b0f62f47e9d3
ryanquinnnelson <ryan.q.nelson@gmail.com>
2016-11-19 21:40:30 -0600

    Added unit test for RecommendedDailyAllowance.

---
+++ App_Engine/src/test/java/AppEngineTests/RecommendedDailyAllowanceTest.java
@@ -0,0 +1,40 @@
+package AppEngineTests;
+
+import com.Engine.Nutrient;
+import com.Engine.NutrientTotal;
+import com.Engine.RecommendedDailyAllowance;
+import com.Engine.StandardRecommendedDailyAllowance;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ryan on 11/19/16.
+ */
+public class RecommendedDailyAllowanceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getAdjustedRDA()
+    {
+        //RecommendedDailyAllowance testRDA = new StandardRecommendedDailyAllowance();
+        fail();
+
+
+        int expected = 60;
+        //int actual = (int) testRDA.getAdjustedRDA(2400).get(8).getAmount();
+        //assertEquals(expected, actual);
+    }
+
+}
